91564f109e97bbf9c9c8a6323c4dc23736b00824
Hiram K <hiram.kamau@outlook.com>
2023-03-12 18:19:37 +0300

    Make immutable

--- src/main/java/com/github/idelstak/fluxfx/example/CounterState.java
+++ src/main/java/com/github/idelstak/fluxfx/example/CounterState.java
@@ -4,33 +4,18 @@ import com.github.idelstak.fluxfx.api.State;
 
 public class CounterState implements State {
 
-    private static CounterState state;
-    private int count;
-
-    private CounterState(int count) {
-        this.count = count;
-    }
+    private final int count;
 
     public static CounterState copy(CounterState state) {
         return from(state.getCount());
     }
 
     public static CounterState from(int count) {
-        if (state == null) {
-            state = new CounterState(count);
-        } else {
-            state.count = count;
-        }
-
-        return state;
+        return new CounterState(count);
     }
 
-    public static CounterState increment() {
-        return from(state.count + 1);
-    }
-
-    public static CounterState decrement() {
-        return from(state.count - 1);
+    private CounterState(int count) {
+        this.count = count;
     }
 
     public int getCount() {

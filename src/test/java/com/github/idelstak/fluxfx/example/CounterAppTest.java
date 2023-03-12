29cf380841ad0083d4c4999926c3d602b19b743b
Hiram K <hiram.kamau@outlook.com>
2023-03-12 19:22:09 +0300

    Update CounterAppTest.java

--- src/test/java/com/github/idelstak/fluxfx/example/CounterAppTest.java
+++ src/test/java/com/github/idelstak/fluxfx/example/CounterAppTest.java
@@ -1,5 +1,7 @@
 package com.github.idelstak.fluxfx.example;
 
+import com.github.idelstak.fluxfx.api.Dispatcher;
+import com.github.idelstak.fluxfx.example.CounterAction.Type;
 import javafx.scene.control.Labeled;
 import javafx.stage.Stage;
 import org.junit.Test;
@@ -8,28 +10,49 @@ import org.testfx.framework.junit.ApplicationTest;
 
 public class CounterAppTest extends ApplicationTest {
 
+    private static final Dispatcher DISPATCHER = new Dispatcher();
+    private static final CounterApp APP = new CounterApp(DISPATCHER);
+
     @Override
     public void start(Stage stage) throws Exception {
-        new CounterApp().start(stage);
+        APP.start(stage);
     }
 
     @Test
-    public void testCounter() {
+    public void testCounterUI() {
+        var count = APP.getState().getCount();
+
         clickOn("#incrementButton");
         Labeled label = lookup("#countLabel").query();
+        
+        count += 1;
 
-        assertThat(label).hasText("1");
+        assertThat(label).hasText(String.valueOf(count));
+        
+        count -= 1;
 
         clickOn("#decrementButton");
-        assertThat(label).hasText("0");
+        assertThat(label).hasText(String.valueOf(count));
 
         for (int i = 0; i < 10; i++) {
+            count += 1;
             clickOn("#incrementButton");
         }
 
-        assertThat(label).hasText("10");
+        assertThat(label).hasText(String.valueOf(count));
+        
+        count -= 1;
 
         clickOn("#decrementButton");
-        assertThat(label).hasText("9");
+        assertThat(label).hasText(String.valueOf(count));
+    }
+
+    @Test
+    public void testCounterActions() {
+        var previousCount = APP.getState().getCount();
+
+        DISPATCHER.dispatch(new CounterAction(Type.INCREMENT));
+
+        assertThat(APP.getState().getCount()).isEqualTo(previousCount + 1);
     }
 }
